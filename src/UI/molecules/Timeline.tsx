62567613c3047599b9c363842262f052a4340533
Pramesh Karki <parameswarkarki@gmail.com>
2022-12-13 19:39:01 +0545

    add: Dynamic data

--- src/UI/molecules/Timeline.tsx
+++ src/UI/molecules/Timeline.tsx
@@ -1,4 +1,5 @@
 import { Icon } from "@iconify/react";
+import timelines, { Timeline as TimelineModel } from "../../data/timeline";
 
 const Timeline = () => {
   return (
@@ -9,101 +10,27 @@ const Timeline = () => {
             <Icon icon={"bi:caret-up-fill"} className="icon" />
           </a>
         </div>
-        <div className="row row-1" id="root-timeline">
-          <section>
-            <i className="icon">
-              <Icon icon={"material-symbols:home"} />
-            </i>
-            <div className="details">
-              <span className="title">Title of Section 1</span>
-              <span>1st Jan 2021</span>
-            </div>
-            <p>
-              Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed
-              qui veroes praesentium maiores, sint eos vero sapiente voluptas
-              debitis dicta dolore.
-            </p>
-          </section>
-        </div>
-        <div className="row row-2">
-          <section>
-            <i className="icon">
-              <Icon icon={"game-icons:graduate-cap"} />
-            </i>
-            <div className="details">
-              <span className="title">Title of Section 2</span>
-              <span>2nd Jan 2021</span>
-            </div>
-            <p>
-              Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed
-              qui veroes praesentium maiores, sint eos vero sapiente voluptas
-              debitis dicta dolore.
-            </p>
-          </section>
-        </div>
-        <div className="row row-1">
-          <section>
-            <i className="icon">
-              <Icon icon={"fa6-solid:user-graduate"} />
-            </i>
-            <div className="details">
-              <span className="title">Title of Section 3</span>
-              <span>3rd Jan 2021</span>
-            </div>
-            <p>
-              Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed
-              qui veroes praesentium maiores, sint eos vero sapiente voluptas
-              debitis dicta dolore.
-            </p>
-          </section>
-        </div>
-        <div className="row row-2">
-          <section>
-            <i className="icon">
-              <Icon icon={"material-symbols:rocket-launch"} />
-            </i>{" "}
-            <div className="details">
-              <span className="title">Title of Section 4</span>
-              <span>4th Jan 2021</span>
-            </div>
-            <p>
-              Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed
-              qui veroes praesentium maiores, sint eos vero sapiente voluptas
-              debitis dicta dolore.
-            </p>
-          </section>
-        </div>
-        <div className="row row-1">
-          <section>
-            <i className="icon">
-              <Icon icon={"ph:paper-plane-fill"} />
-            </i>{" "}
-            <div className="details">
-              <span className="title">Title of Section 5</span>
-              <span>5th Jan 2021</span>
-            </div>
-            <p>
-              Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed
-              qui veroes praesentium maiores, sint eos vero sapiente voluptas
-              debitis dicta dolore.
-            </p>
-          </section>
-        </div>
-        <div className="row row-2">
-          <section>
-            <i className="icon">
-              <Icon icon={"mdi:map-marker"} />
-            </i>
-            <div className="details">
-              <span className="title">Title of Section 6</span>
-              <span>6th Jan 2021</span>
-            </div>
-            <p>
-              Lorem ipsum dolor sit ameters consectetur adipisicing elit. Sed
-              qui veroes praesentium maiores, sint eos vero sapiente voluptas
-              debitis dicta dolore.
-            </p>
-          </section>
+        <div>
+          {timelines.map((timeline: TimelineModel, index: number) => {
+            return (
+              <div
+                className={`row ${index % 2 === 0 ? "row-1" : "row-2"}`}
+                key={index}
+                id={index === 0 ? "root-timeline" : undefined}
+              >
+                <section>
+                  <i className="icon">
+                    <Icon icon={timeline.icon} />
+                  </i>
+                  <div className="details">
+                    <span className="title">{timeline.title}</span>
+                    <span>{timeline.date}</span>
+                  </div>
+                  <p>{timeline.description}</p>
+                </section>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
