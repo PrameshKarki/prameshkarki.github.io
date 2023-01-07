787cd3913f86eae98f64bafa0769f9f29a667b41
Pramesh Karki <parameswarkarki@gmail.com>
2023-01-07 21:30:27 +0545

    add: Google analytics & Meta tags

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 // import "aos/dist/aos.css";
 import ReactDOM from "react-dom/client";
+import ReactGA from "react-ga";
 import "./assets/scss/main.scss";
 import App from "./UI/App";
 
+ReactGA.initialize("G-S2GQWW0KR7");
+ReactGA.pageview(window.location.pathname + window.location.search);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <React.StrictMode>
   // </React.StrictMode>
